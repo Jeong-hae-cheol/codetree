dca67a3ea2f6e7ee20bd9c3696a5a216b305b88b
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-27 17:42:39 +0900

    [Passed] 4가지 관계연산자 (88ms, 9MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader  br = new BufferedReader (
+            new InputStreamReader(System.in)
+        );
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if(A >= B) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        if(A > B) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        if(A <= B) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        if(A < B) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        // Please write your code here.
+    }
+}
