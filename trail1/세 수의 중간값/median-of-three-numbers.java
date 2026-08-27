ea3a7e4c199f6b734d6d58d5e1768b9fe6a33b0b
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-27 22:26:08 +0900

    [Passed] 세 수의 중간값 (86ms, 9MB)

---
+++ trail1/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(b > a && b < c) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
