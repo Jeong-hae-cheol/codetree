3ba637b49cf0eb64506d02bc61356cede0b3bae0
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-27 22:18:32 +0900

    [Passed] 세 정수의 최솟값 (88ms, 9MB)

---
+++ trail1/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int answer = Math.min(a,b);
+        answer = Math.min(answer,c);
+
+        System.out.println(answer);
+
+        
+        // Please write your code here.
+    }
+}
