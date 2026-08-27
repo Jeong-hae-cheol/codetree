e623eef4c859cb8e8c6750000a42fb6523ee381a
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-27 23:05:02 +0900

    [Passed] 1부터 n까지 출력 (111ms, 9MB)

---
+++ trail1/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
