7ddf4c6d78ecf482a823cc41cc2f5816d8cf1f36
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-27 22:50:34 +0900

    [Passed] 최댓값 구하기 (89ms, 9MB)

---
+++ trail1/최댓값 구하기/maximum-value.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        
+        int answer = Math.max(a,b);
+        answer = Math.max(answer, c);
+
+       
+
+        System.out.println(answer);
+    }
+}
