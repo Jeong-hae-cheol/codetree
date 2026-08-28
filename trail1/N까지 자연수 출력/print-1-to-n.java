2e0a4910ec689d3affd7d9e152c2bcd40e74a8fa
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-28 11:14:37 +0900

    [Passed] N까지 자연수 출력 (106ms, 9MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        // int B = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= A; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
