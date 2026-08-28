97468cb6c553e1a6b084b3fe8575e6c52172f3a0
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-28 10:43:07 +0900

    [Passed] 증가시키며 출력하기 (94ms, 9MB)

---
+++ trail1/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        for(int i = 5; i <= 17; i+=2) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
