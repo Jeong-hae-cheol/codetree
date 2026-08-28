5e019024c93cd0eec39b9d93b9e0fc27883ac13b
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-28 11:36:22 +0900

    [Passed] n부터 1까지 정수 출력 (101ms, 9MB)

---
+++ trail1/n부터 1까지 정수 출력/print-n-to-1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        // int B = Integer.parseInt(st.nextToken());
+
+        while(A > 0) {
+            System.out.printf("%d ", A);
+            A--;
+        }
+    }
+}
