d5e47a5f77d131a4eb7f9bd3f194eb057cc11606
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-28 16:07:19 +0900

    [Passed] A 부터 B 까지 (96ms, 9MB)

---
+++ trail1/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        while(A <= B) {
+            System.out.printf("%d ", A);
+            if(A % 2 == 0) {
+                A += 3;
+            } else {
+                A *= 2;
+            }
+        }
+    }
+}
