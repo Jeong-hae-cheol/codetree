74388d216aa69101c8bf05693a6ff9d20005b3ac
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-28 14:33:36 +0900

    [Passed] 자연수면 출력 (92ms, 9MB)

---
+++ trail1/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        if(A > 0) {
+            for(int i = 0; i < B; i++) {
+                System.out.printf("%d", A);
+            }
+        } else {
+            System.out.printf("%d", 0);
+        }
+    }
+}
