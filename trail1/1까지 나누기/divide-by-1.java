88a8c29c9d7b2cf11d9a99d7432d4316605474fd
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-29 16:11:14 +0900

    [Passed] 1까지 나누기 (91ms, 9MB)

---
+++ trail1/1까지 나누기/divide-by-1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // int A = Integer.parseInt(st.nextToken());
+        // int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 1;
+        // int answer = 0;
+
+        for(int i = 1; i <= 100; i++) {
+            N /= i;            
+            if(N <= 1) {
+                System.out.println(i);
+                break;
+            }
+        }
+
+        // System.out.println(answer);
+    }
+}
