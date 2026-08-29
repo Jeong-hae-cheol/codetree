32ca4896dd396979ea3d5f16fe2083541ac23cbc
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-29 15:52:13 +0900

    [Passed] 총합 구하기 (91ms, 9MB)

---
+++ trail1/총합 구하기/find-the-total.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int sum = 0;        
+
+        for (int i = A; i <= B; i++) {          
+            if(i % 6 == 0 && i % 8 != 0) {
+                sum += i;
+            }            
+        }
+                        
+        System.out.println(sum);
+    }
+}
