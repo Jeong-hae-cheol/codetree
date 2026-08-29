7705915b3f80856c8051592d7edee2e1c5f53b29
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-29 16:04:26 +0900

    [Passed] 조건에 부합하는 수 (100ms, 9MB)

---
+++ trail1/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // int A = Integer.parseInt(st.nextToken());
+        // int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+
+        // int answer = 0;
+
+        for(int i = 1; i <= N; i++) {
+            if(i%2==0 && i%4!=0)
+                continue;
+            if(i/8%2 == 0)
+                continue;
+            if(i%7 < 4)
+                continue;
+            System.out.printf("%d ", i);
+        }
+
+        // System.out.println(answer);
+    }
+}
