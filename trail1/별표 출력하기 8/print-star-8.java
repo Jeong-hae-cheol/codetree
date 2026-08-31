82649b4f8b79fac1666ace2b6493501f77746d8f
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-31 21:43:46 +0900

    [Passed] 별표 출력하기 8 (99ms, 9MB)

---
+++ trail1/별표 출력하기 8/print-star-8.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());        
+
+        for(int i = 1; i <= N; i++) {
+            if(i % 2 == 0) {
+                for(int j = 0; j < i; j++) {
+                    System.out.printf("* ");
+                }
+            } else {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
