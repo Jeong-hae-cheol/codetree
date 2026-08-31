94cf29d6bbb041981fcb46d8b62865a5c9a8ee93
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-31 16:17:01 +0900

    [Passed] 3N + 1 수열 (91ms, 9MB)

---
+++ trail1/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while(n > 1) {
+            if(n%2 == 0) {
+                n /= 2;
+            } else {
+                n = 3*n+1;
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
