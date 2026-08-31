890a24b0ab25c2fc933d0556a4ab2df21f382be3
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-31 22:14:39 +0900

    [Passed] 별표 출력하기 12 (97ms, 9MB)

---
+++ trail1/별표 출력하기 12/print-star-12.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());                
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {                
+                if(i == 0) {
+                    System.out.printf("* ");
+                } else if(j % 2 != 0 && i <= j) {
+                    System.out.printf("* ");
+                } else {
+                    System.out.printf("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
