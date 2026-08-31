6fa1d1ea45104c30290480359a6aeb10c7d7be8a
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-08-31 21:51:07 +0900

    [Passed] 별표 출력하기 10 (97ms, 9MB)

--- trail1/별표 출력하기 10/print-star-10.java
+++ trail1/별표 출력하기 10/print-star-10.java
@@ -17,15 +17,13 @@ public class Main {
 
         for(int i = 0; i < N*2; i++) {
             if(i % 2 == 0) {
-                for(int j = 0; j <= n1; j++) {
+                for(int j = 0; j <= i/2; j++) {
                     System.out.printf("* ");
-                }
-                n1++;
+                }                
             } else {
-                for(int j = n2; j > 0; j--) {
+                for(int j = 0; j < N - i/2; j++) {
                     System.out.printf("* ");
-                }
-                n2--;
+                }                
             }
             System.out.println();
         }
