40afa57f20bb1cb1b051df21f67223ec4419a2a8
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-01 17:15:59 +0900

    [Passed] 십의 자리 숫자 (101ms, 9MB)

---
+++ trail1/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // int N = Integer.parseInt(st.nextToken());        
+
+        // st = new StringTokenizer(br.readLine());
+
+        int[] cnt = new int[10];
+        for(int i = 0; i < 100; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(num == 0) {
+                break;
+            }
+            cnt[num/10]++;
+        }
+
+        for(int i = 1; i <= 9; i++) {            
+            System.out.println(i + " - " + cnt[i]);
+        }        
+    }
+}
