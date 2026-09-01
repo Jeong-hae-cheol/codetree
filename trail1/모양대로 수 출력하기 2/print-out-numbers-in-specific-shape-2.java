6f24e359cc2f4d456c9b8a6d2ba490d1de2700c2
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-01 11:48:59 +0900

    [Passed] 모양대로 수 출력하기 2 (94ms, 9MB)

---
+++ trail1/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        // int B = Integer.parseInt(st.nextToken());
+
+        int cnt = 2;
+
+        for(int i = 1; i <= N; i++) {            
+            for(int j = 1; j <= N; j++) {
+                System.out.printf("%d ", cnt);
+                cnt = (cnt+2)%10;
+                if(cnt == 0) {
+                    cnt+=2;
+                }
+            }            
+            System.out.println();
+        }
+    }
+}
