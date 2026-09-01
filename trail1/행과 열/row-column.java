7cae314fa03ae39dece520923974fedbaebf7efd
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-01 11:40:21 +0900

    [Passed] 행과 열 (97ms, 9MB)

---
+++ trail1/행과 열/row-column.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());                
+        int B = Integer.parseInt(st.nextToken());                
+
+        for(int i = 1; i <= A; i++) {            
+            for(int j = 1; j <= B; j++) {                
+                System.out.printf("%d ", i*j);
+            }            
+            System.out.println();
+        }
+    }
+}
