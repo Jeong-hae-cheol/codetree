c34bc0a3697fc8ddd77ecb65791346d4da878a32
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-01 14:47:29 +0900

    [Passed] 완전수 (95ms, 9MB)

---
+++ trail1/완전수/perfect-number.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        int cnt = 0;
+
+        for(int i = A; i <= B; i++) {
+            int sum = 0;            
+            for(int j = 1; j < i; j++) {
+                if(i % j == 0) {
+                    sum += j;
+                }
+            }
+            if(sum == i) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
