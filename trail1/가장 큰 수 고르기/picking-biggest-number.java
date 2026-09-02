df95389d114f7ecb9a280bd43510b0359c245506
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-02 16:56:24 +0900

    [Passed] 가장 큰 수 고르기 (92ms, 9MB)

---
+++ trail1/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // int N = Integer.parseInt(st.nextToken());                
+
+        int answer = 0;
+
+        for(int i = 0; i < 10; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            answer = Math.max(n, answer);
+        }
+            
+        System.out.println(answer);
+    }
+}
