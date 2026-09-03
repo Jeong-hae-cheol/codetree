b608f1853bedd271c764a0740539ce4c00d51a1a
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-03 09:40:06 +0900

    [Passed] 배열의 합 (89ms, 9MB)

---
+++ trail1/배열의 합/sum-of-array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // int N = Integer.parseInt(st.nextToken());
+        
+        
+
+        for(int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for(int j = 0; j < 4; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            System.out.println(sum);
+        }
+    }
+}
