9a49fcfc47eef715a4248aee68b6dd944afcf716
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-04 17:30:47 +0900

    [Passed] 문자열 추가하기 (90ms, 9MB)

---
+++ trail1/문자열 추가하기/add-spring.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        // Please write your code here.
+
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String str = br.readLine();        
+        
+        System.out.print(str+"Hello");
+    }
+}
