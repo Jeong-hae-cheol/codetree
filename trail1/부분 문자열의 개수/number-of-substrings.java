8f8ffa05e28035e864d1b1898dcbe915d3ab205a
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-05 19:34:29 +0900

    [Passed] 부분 문자열의 개수 (92ms, 9MB)

---
+++ trail1/부분 문자열의 개수/number-of-substrings.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(br.readLine());
+        String str = br.readLine();
+        
+        int cnt = 0;
+
+        for(int i = 0; i <= sb.length() - str.length(); i++) {
+            String s = sb.substring(i, str.length()+i);
+            if(s.equals(str)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
