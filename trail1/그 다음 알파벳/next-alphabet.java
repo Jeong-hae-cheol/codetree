3429f9a71360ab2a82a4183f0925814572083876
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-07 13:18:06 +0900

    [Passed] 그 다음 알파벳 (100ms, 9MB)

---
+++ trail1/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        char a = st.nextToken().charAt(0);        
+        if(a == 'z') {
+            a = 'a';
+        } else {
+            a++;
+        }
+
+        System.out.printf("%c", a);
+    }
+}
