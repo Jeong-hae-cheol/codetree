37f4cdf06eeca3ce034ccf5e37885637d0209417
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-07 14:29:55 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (306ms, 15MB)

---
+++ trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowNum = sc.nextInt();
+        int colNum = sc.nextInt();
+        // Please write your code here.        
+
+        printSqaure(rowNum, colNum);
+    }
+
+    public static void printSqaure(int r, int c) {
+        for(int i = 0 ; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                System.out.printf("1");
+            }
+            System.out.println();
+        }
+    }
+}
