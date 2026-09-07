1f136c3adb3f6a3da85d8f6947bf973a7ca742d5
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-07 14:28:10 +0900

    [Passed] 반복 출력하기 (89ms, 9MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {            
+            System.out.println("12345^&*()_");
+        }
+    }
+
+    public static void printStars(int n) {        
+    }
+}
