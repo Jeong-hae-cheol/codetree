6eb5d431a2dc1fd70ca26125cf7671f219774cdf
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-08 13:53:58 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (130ms, 10MB)

---
+++ trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(solve(n));
+    }
+
+    public static int solve(int n) {
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+
+        return solve(n-1) + solve(n-2);
+    }
+}
