9ace4e4a07ea2f78c1482fb09dc6fcfe7e6089a7
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-21 17:10:23 +0900

    [Passed] 빙빙 돌며 정수로 사각형 채우기 2 (123ms, 10MB)

---
+++ trail2/빙빙 돌며 정수로 사각형 채우기 2/snail-number-square-2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    static int n;
+    static int m;
+    static int[][] board;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        // Please write your code here.
+        board = new int [n][m];
+        int[] dr = new int[]{1, 0, -1, 0};
+        int[] dc = new int[]{0, 1, 0, -1};
+        int dir = 0;
+        int r = 0;
+        int c = 0;
+
+        for (int i = 1; i <= n*m; i++) {
+            board[r][c] = i;
+
+            if(!inRange(r+dr[dir],c+dc[dir]) || board[r+dr[dir]][c+dc[dir]] != 0) {
+                dir = (dir+1) % 4;
+            }
+
+            r = r+dr[dir];
+            c = c+dc[dir];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean inRange(int r, int c) {
+        if(r < 0 || r >= n || c < 0 || c >= m) {
+            return false;
+        }
+        return true;
+    }
+}
