c900d0a6b78527b99e7102b36a2180a164b4d703
Jeong-hae-cheol <84774453+Jeong-hae-cheol@users.noreply.github.com>
2026-09-23 11:44:34 +0900

    [Passed] 씨 오 더블유 2 (135ms, 10MB)

---
+++ trail2/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        // Please write your code here.
+        int ans = 0;
+        for (int i = 0; i < n-2; i++) {
+            if(str.charAt(i) != 'C')
+                continue;
+            for (int j = i+1; j < n-1; j++) {
+                if(str.charAt(j) != 'O')
+                    continue;
+                for (int k = j+1; k < n; k++) {
+                    if(str.charAt(k) != 'W')
+                        continue;
+                    ans++;
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
